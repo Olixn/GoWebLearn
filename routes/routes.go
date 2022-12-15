e3340d956f172e920979071216c4d734b4b01ee3
Olixn <95512865+Olixn@users.noreply.github.com>
2022-12-15 23:27:03 +0800

    init

---
+++ routes/routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/Olixn/GoWebLearn/logger"
+	"github.com/Olixn/GoWebLearn/settings"
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(mode string) *gin.Engine {
+	if mode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.GET("/version", func(c *gin.Context) {
+		c.String(http.StatusOK, settings.Conf.Version)
+	})
+	
+	return r
+}
